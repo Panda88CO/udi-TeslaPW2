4e5323b82f0638a4a1f81081259840516773b4b1
Panda88CO <christian.olgaard@gmail.com>
2024-02-13 10:07:25 -0800

    Update TeslaPWOauth.py

--- TeslaPWOauth.py
+++ TeslaPWOauth.py
@@ -191,7 +191,7 @@ class TeslaCloud(OAuth):
         oauthSettingsUpdate['token_parameters']['audience'] = self.Endpoint
         oauthSettingsUpdate['token_parameters']['client_id'] = '6e635ec38dc4-4d2a-a35e-f164b51f3d96'
         oauthSettingsUpdate['token_parameters']['client_secret'] = '!GBQbeMcua2zZ5WERTSF3KX@'
-        oauthSettingsUpdate['token_parameters']['addRedirect'] = True
+        #oauthSettingsUpdate['token_parameters']['addRedirect'] = True
         self.updateOauthSettings(oauthSettingsUpdate)
         time.sleep(0.1)
         temp = self.getOauthSettings()
