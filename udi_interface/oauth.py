23f7d78be4033cb64cc19c4dff67f21eea24266f
Panda88CO <christian.olgaard@gmail.com>
2024-03-04 15:10:17 -0800

    Update oauth.py

--- udi_interface/oauth.py
+++ udi_interface/oauth.py
@@ -116,7 +116,7 @@ class OAuth:
                 LOGGER.info(f"Access tokens: Token is expired since { expiry }. Initiating refresh.")
                 self._oAuthTokensRefresh()
             else:
-                LOGGER.info(f"Access tokens: Token is still valid until { expiry }, no need to refresh")
+                logger.info(f"Access tokens: Token is still valid until { expiry }, no need to refresh")
 
             return self._oauthTokens.get('access_token')
         else:
