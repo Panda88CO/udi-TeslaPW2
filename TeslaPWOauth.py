7ca3115ce980fb64a68d539b713543827c649f14
Panda88CO <christian.olgaard@gmail.com>
2024-03-26 17:56:58 -0700

    Update TeslaPWOauth.py

--- TeslaPWOauth.py
+++ TeslaPWOauth.py
@@ -286,7 +286,7 @@ class teslaAccess(udi_interface.OAuth):
             expiry = self._oauthTokens.get('expiry')
 
             # If expired or expiring in less than 60 seconds, refresh
-            if (expiry is None or datetime.fromisoformat(expiry) - timedelta(seconds=60) < datetime.now()):
+            if ((expiry is None or datetime.fromisoformat(expiry) - timedelta(seconds=60) < datetime.now()) and self.authendication_done):
 
                 logging.info(f"Access tokens: Token is expired since { expiry }. Initiating refresh.")
                 self._oAuthTokensRefresh()
