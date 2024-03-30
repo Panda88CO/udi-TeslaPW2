aed05d8b2145256be8e5590e2f3c192d715d3015
Panda88CO <christian.olgaard@gmail.com>
2024-03-29 17:17:30 -0700

    Update TeslaPWmain.py

--- TeslaPWmain.py
+++ TeslaPWmain.py
@@ -18,7 +18,7 @@ except ImportError:
     logging.basicConfig(level=30)
 
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 class TeslaPWController(udi_interface.Node):
     from  udiLib import node_queue, wait_for_node_done, mask2key, heartbeat
 
