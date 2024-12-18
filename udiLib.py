5a2b426c4eb0d285b874b18c0f75efe90c84a3cb
Panda88CO <christian.olgaard@gmail.com>
2024-12-18 15:42:15 -0800

    Update udiLib.py

--- udiLib.py
+++ udiLib.py
@@ -135,7 +135,7 @@ def PW_setDriver(self, key, value, Unit=None):
         if Unit:
             self.node.setDriver(key, value, True, True, Unit)
         else:
-            self.node.setDriver(key, value)
+            self.node.setDriver(key, value, True, True)
 
 
 def send_rel_temp_to_isy(self, temperature, stateVar):
